82191f259d7b00da159765fdc3813c769aabc958
SharonNavarro <shanmowrk@gmail.com>
2024-12-13 19:37:03 -0500

    test: utilities

--- eslint.config.js
+++ eslint.config.js
@@ -9,6 +9,48 @@ const eslintPluginPrettierRecommended = require('eslint-plugin-prettier/recommen
 
 module.exports = tseslint.config(
   {
+    ignores: [
+      // Compiled output
+      "dist",
+      "tmp",
+      "out-tsc",
+      "bazel-out",
+
+      // Node
+      "node_modules",
+      "npm-debug.log",
+      "yarn-error.log",
+
+      // IDEs and editors
+      ".idea/",
+      ".project",
+      ".classpath",
+      ".c9/",
+      "*.launch",
+      ".settings/",
+      "*.sublime-workspace",
+
+      // Visual Studio Code
+      ".vscode/*",
+      "!.vscode/settings.json",
+      "!.vscode/tasks.json",
+      "!.vscode/launch.json",
+      "!.vscode/extensions.json",
+      ".history/*",
+
+      // Miscellaneous
+      ".angular/cache",
+      ".sass-cache/",
+      "connect.lock",
+      "coverage",
+      "libpeerconnection.log",
+      "testem.log",
+      "typings",
+
+      // System files
+      ".DS_Store",
+      "Thumbs.db"
+    ],
     files: ['**/*.ts'],
     plugins: {
       // @ts-ignore
