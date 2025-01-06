7d7eea0101073636eda2c683a906ee21977dd77d
SharonNavarro <shanmowrk@gmail.com>
2025-01-06 17:10:33 -0500

    refactor(user-service): provide comments. test: rxjs stream behaviour

--- src/app/shared/services/users.service.ts
+++ src/app/shared/services/users.service.ts
@@ -1,30 +1,37 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
 
 import { UserInterface } from '../types/user.interface';
-// import { UtilsService } from './utils.service';
+import { UtilsService } from './utils.service';
+
 @Injectable()
 export class UsersService {
-  // public utilsService = inject(UtilsService);
-  // public users: UserInterface[] = [];
+  // Inject the UtilsService using Angular's inject function
+  public utilsService = inject(UtilsService);
 
-  users$ = new BehaviorSubject<UserInterface[]>([]);
+  // BehaviorSubject to manage the state of users
+  // It starts with an empty array and allows emitting and observing new values
+  public users$ = new BehaviorSubject<UserInterface[]>([]);
 
+  // Method to add a user to the stream
   public addUser(user: UserInterface): void {
-    // this.users = [...this.users, user];
+    // Update the stream with the current users array plus the new user
     this.users$.next([...this.users$.getValue(), user]);
   }
 
+  // Method to remove a user from the stream based on user ID
   public removeUser(userId: string): void {
-    // const updatedUsers = this.users.filter(user => userId !== user.id);
-    // this.users = updatedUsers;
+    // Filter the current users array to exclude the user with the specified ID
     const updatedUsers = this.users$
       .getValue()
       .filter(user => userId !== user.id);
+    // Emit the updated array
     this.users$.next(updatedUsers);
   }
 
-  // public getUsernames(): string[] {
-  //   return this.utilsService.pluck(this.users, 'name');
-  // }
+  // Method to get usernames using the pluck method from UtilsService
+  public getUsernames(): string[] {
+    // Use the current value of the stream to extract the usernames
+    return this.utilsService.pluck(this.users$.getValue(), 'name');
+  }
 }
